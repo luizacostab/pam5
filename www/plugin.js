3bcd926a23e5c07271c23095d26a0b9652e968d3
luizacostab <lu.izacosta@hotmail.com>
2020-07-13 22:17:04 +0900

    Project's Initial Commit

---
+++ www/plugin.js
@@ -0,0 +1,60 @@
+function camera(){
+  navigator.camera.getPicture(onSuccess, onFail, { 
+    quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI 
+  });
+  function onSuccess(imageURI) {
+      var image = document.getElementById('img');
+      image.src = imageURI;
+  }
+  function onFail(message) {
+      alert('Failed because: ' + message);
+  }
+}
+function barcode(){
+  cordova.plugins.barcodeScanner.scan(
+      function (result) {
+        if(result.cancelled == true){
+          result.cancelled = 'foi cancelado'
+        } else{
+          result.cancelled = 'não foi cancelado'
+        }
+          alert(
+                "Resultado: " + result.text + "\n" +
+                "Formato: " + result.format + "\n" +
+                "Cancelado: " + result.cancelled);
+      },
+      function (error) {
+          alert("Escaneamento falhou: " + error);
+      },
+      {
+          preferFrontCamera : false, 
+          showFlipCameraButton : true, 
+          showTorchButton : true, 
+          torchOn: true, 
+          saveHistory: true, 
+          prompt : "Place a barcode inside the scan area", 
+          resultDisplayDuration: 500, 
+          formats : "QR_CODE,PDF_417", 
+          orientation : "landscape", 
+          disableAnimations : true, 
+          disableSuccessBeep: false 
+      }
+   );
+}
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    alert('Tipo de conexão: ' + states[networkState]);
+}
+checkConnection();
